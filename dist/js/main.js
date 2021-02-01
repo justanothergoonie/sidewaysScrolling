775034481ad388472febf1a28724e4ef8e8d4714
Cody J Powell <codyj.powell@outlook.com>
2021-02-01 11:53:48 -0500

    sideway scrolling issues

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,93 +3,55 @@
 console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
 gsap.registerPlugin(ScrollTrigger);
 var sections = gsap.utils.toArray('.panel');
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: 'none',
-  scrollTrigger: {
-    trigger: '.container',
-    pin: true,
-    scrub: 1,
-    // snap: 1 / (sections.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: function end() {
-      return '+=' + document.querySelector('.container').offsetWidth;
-    }
-  }
-});
-sections.forEach(function (sct, i) {
-  ScrollTrigger.create({
-    trigger: sct,
-    start: 'top top-=' + (sct.offsetLeft - window.innerWidth / 2) * (document.querySelector('.container').offsetWidth / (sct.offsetWidth * (sections.length - 1))),
-    end: '+=' + sct.offsetWidth * (document.querySelector('.container').offsetWidth / (sct.offsetWidth * (sections.length - 1))),
-    toggleClass: {
-      targets: sct,
-      className: 'active'
-    }
+var NUM_PANELS = 6;
+var PANEL_RATIO = 16 / 9;
+var experienceWrapper = document.querySelector('.experience-wrapper');
+var experience = experienceWrapper.querySelector('.experience');
+var experiencePadder = experienceWrapper.querySelector('.experience-padder');
+var panels = experienceWrapper.querySelectorAll('.panel');
+var timeline = gsap.timeline({
+  duration: 100,
+  paused: true
+}); // set up a 100s timeline
+
+var resizeStuff = function resizeStuff() {
+  var onePanelHeight = panels[0].offsetHeight; // just ask the first panel how tall it is, to find what CSS has currently calculated its height to be
+
+  var onePanelWidth = onePanelHeight * PANEL_RATIO; // figure out what its width should be to maintain the chosen ratio
+
+  experience.style.width = onePanelWidth * NUM_PANELS + 'px'; // set the whole sideways scrolling thing to NUM_PANELS times the width of one panel
+
+  panels.forEach(function (panel) {
+    panel.style.width = onePanelWidth + 'px'; // set the panels to that single panel width too
   });
-}); // gsap.to('.c', {
-// 	horizontal: true,
-// 	scrollTrigger: {
-// 		trigger: '.c',
-// 		horizontal: true,
-// 		start: 'left start',
-// 		end: 'right center',
-// 		scrub: true,
-// 		// end: () => '=+' + document.querySelector('.c').offsetWidth,
-// 		markers: true,
-// 		toggleActions: 'restart pause reverse pause',
-// 	},
-// 	x: 400,
-// 	rotation: 360,
-// 	ease: 'none',
-// 	duration: 3,
-// });
-// gsap.to('.ghost1', {
-// 	scrollTrigger: {
-// 		trigger: '.c',
-// 		horizontal: true,
-// 		start: 'left center',
-// 		end: 'right center',
-// 		scrub: 2,
-// 		// end: () => '=+' + document.querySelector('.c').offsetWidth,
-// 		markers: true,
-// 		toggleActions: 'restart pause reverse pause',
-// 	},
-// 	x: 400,
-// 	rotation: 360,
-// 	ease: 'none',
-// 	duration: 3,
-// });
-// gsap.to('.ghost2', {
-// 	scrollTrigger: {
-// 		trigger: '.c',
-// 		horizontal: true,
-// 		start: 'left center',
-// 		end: 'right center',
-// 		scrub: 4,
-// 		// end: () => '=+' + document.querySelector('.c').offsetWidth,
-// 		markers: true,
-// 		toggleActions: 'restart pause reverse pause',
-// 	},
-// 	x: 400,
-// 	rotation: 360,
-// 	ease: 'none',
-// 	duration: 3,
-// });
-// gsap.to('.ghost3', {
-// 	scrollTrigger: {
-// 		trigger: '.c',
-// 		horizontal: true,
-// 		start: 'left center',
-// 		end: 'right center',
-// 		scrub: 8,
-// 		// end: () => '=+' + document.querySelector('.c').offsetWidth,
-// 		markers: true,
-// 		toggleActions: 'restart pause reverse pause',
-// 	},
-// 	x: 400,
-// 	rotation: 360,
-// 	ease: 'none',
-// 	duration: 3,
-// });
+  experiencePadder.style.height = onePanelWidth * NUM_PANELS + 'px'; // set the hidden padding element to be tall enough that we can scroll down through NUM_PANELS worth of panels
+};
+
+window.addEventListener('resize', resizeStuff);
+window.addEventListener('orientationchange', resizeStuff);
+resizeStuff(); // window.addEventListener('scroll', () => {
+
+gsap.ticker.add(function () {
+  var topLimit = experienceWrapper.getBoundingClientRect().top;
+  console.log(topLimit);
+
+  if (topLimit < 0) {
+    experience.style.top = -topLimit + 'px'; // keeps the panels in place
+
+    experience.style.left = topLimit + 'px'; // keeps the panels in place
+  } else {
+    experience.style.top = 0 + 'px';
+  }
+
+  var percent = -topLimit / (experiencePadder.offsetHeight - panels[0].offsetHeight);
+  timeline.progress(percent);
+}); // gsap animations
+// put a tween on the timeline
+
+timeline.to(['.painting', '.table', '.man', '.gun', '.news'], {
+  duration: 5,
+  // takes 2 seconds
+  left: '50%',
+  top: '50%'
+}, 0); // starts at the 1 second mark
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -6,100 +6,53 @@ gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray('.panel');
 
-gsap.to(sections, {
-	xPercent: -100 * (sections.length - 1),
-	ease: 'none',
-	scrollTrigger: {
-		trigger: '.container',
-		pin: true,
-		scrub: 1,
-		// snap: 1 / (sections.length - 1),
-		// base vertical scrolling on how wide the container is so it feels more natural.
-		end: () => '+=' + document.querySelector('.container').offsetWidth,
-	},
-});
-sections.forEach((sct, i) => {
-	ScrollTrigger.create({
-		trigger: sct,
-		start:
-			'top top-=' +
-			(sct.offsetLeft - window.innerWidth / 2) *
-				(document.querySelector('.container').offsetWidth /
-					(sct.offsetWidth * (sections.length - 1))),
-		end:
-			'+=' +
-			sct.offsetWidth *
-				(document.querySelector('.container').offsetWidth /
-					(sct.offsetWidth * (sections.length - 1))),
-		toggleClass: { targets: sct, className: 'active' },
+const NUM_PANELS = 6;
+const PANEL_RATIO = 16 / 9;
+
+const experienceWrapper = document.querySelector('.experience-wrapper');
+const experience = experienceWrapper.querySelector('.experience');
+const experiencePadder = experienceWrapper.querySelector('.experience-padder');
+const panels = experienceWrapper.querySelectorAll('.panel');
+const timeline = gsap.timeline({ duration: 100, paused: true }); // set up a 100s timeline
+
+let resizeStuff = () => {
+	let onePanelHeight = panels[0].offsetHeight; // just ask the first panel how tall it is, to find what CSS has currently calculated its height to be
+	let onePanelWidth = onePanelHeight * PANEL_RATIO; // figure out what its width should be to maintain the chosen ratio
+	experience.style.width = onePanelWidth * NUM_PANELS + 'px'; // set the whole sideways scrolling thing to NUM_PANELS times the width of one panel
+	panels.forEach((panel) => {
+		panel.style.width = onePanelWidth + 'px'; // set the panels to that single panel width too
 	});
+	experiencePadder.style.height = onePanelWidth * NUM_PANELS + 'px'; // set the hidden padding element to be tall enough that we can scroll down through NUM_PANELS worth of panels
+};
+
+window.addEventListener('resize', resizeStuff);
+window.addEventListener('orientationchange', resizeStuff);
+resizeStuff();
+
+// window.addEventListener('scroll', () => {
+gsap.ticker.add(() => {
+	let topLimit = experienceWrapper.getBoundingClientRect().top;
+	console.log(topLimit);
+	if (topLimit < 0) {
+		experience.style.top = -topLimit + 'px'; // keeps the panels in place
+		experience.style.left = topLimit + 'px'; // keeps the panels in place
+	} else {
+		experience.style.top = 0 + 'px';
+	}
+	let percent =
+		-topLimit / (experiencePadder.offsetHeight - panels[0].offsetHeight);
+	timeline.progress(percent);
 });
-// gsap.to('.c', {
-// 	horizontal: true,
-// 	scrollTrigger: {
-// 		trigger: '.c',
-// 		horizontal: true,
-// 		start: 'left start',
-// 		end: 'right center',
-// 		scrub: true,
-// 		// end: () => '=+' + document.querySelector('.c').offsetWidth,
-// 		markers: true,
 
-// 		toggleActions: 'restart pause reverse pause',
-// 	},
-// 	x: 400,
-// 	rotation: 360,
-// 	ease: 'none',
-// 	duration: 3,
-// });
+// gsap animations
 
-// gsap.to('.ghost1', {
-// 	scrollTrigger: {
-// 		trigger: '.c',
-// 		horizontal: true,
-// 		start: 'left center',
-// 		end: 'right center',
-// 		scrub: 2,
-// 		// end: () => '=+' + document.querySelector('.c').offsetWidth,
-// 		markers: true,
-
-// 		toggleActions: 'restart pause reverse pause',
-// 	},
-// 	x: 400,
-// 	rotation: 360,
-// 	ease: 'none',
-// 	duration: 3,
-// });
-// gsap.to('.ghost2', {
-// 	scrollTrigger: {
-// 		trigger: '.c',
-// 		horizontal: true,
-// 		start: 'left center',
-// 		end: 'right center',
-// 		scrub: 4,
-// 		// end: () => '=+' + document.querySelector('.c').offsetWidth,
-// 		markers: true,
-
-// 		toggleActions: 'restart pause reverse pause',
-// 	},
-// 	x: 400,
-// 	rotation: 360,
-// 	ease: 'none',
-// 	duration: 3,
-// });
-// gsap.to('.ghost3', {
-// 	scrollTrigger: {
-// 		trigger: '.c',
-// 		horizontal: true,
-// 		start: 'left center',
-// 		end: 'right center',
-// 		scrub: 8,
-// 		// end: () => '=+' + document.querySelector('.c').offsetWidth,
-// 		markers: true,
-// 		toggleActions: 'restart pause reverse pause',
-// 	},
-// 	x: 400,
-// 	rotation: 360,
-// 	ease: 'none',
-// 	duration: 3,
-// });
+// put a tween on the timeline
+timeline.to(
+	['.painting', '.table', '.man', '.gun', '.news'],
+	{
+		duration: 5, // takes 2 seconds
+		left: '50%',
+		top: '50%',
+	},
+	0
+); // starts at the 1 second mark
